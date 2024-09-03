cd02c70bccddec7f3cefa0fc27b01a648b6200c8
123Mismail <musafirbaltistani786@gmail.com>
2024-09-03 06:56:28 -0700

    first commit

---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Heading() {
+  return (
+    <div className='mx-auto text-center text-3xl font-medium mt-6 mb-4'><h1> Quiz App </h1></div>
+  )
+}
+
+export default Heading
